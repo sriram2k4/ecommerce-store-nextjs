de69f3448b48c8299f90a7f2d6e2b4d1c63a99c5
sriram2k4 <sriramthehacker01@gmail.com>
2024-04-28 15:42:13 +0530

    Admin dashboard done

---
+++ src/app/admin/page.tsx
@@ -0,0 +1,104 @@
+import {
+  Card,
+  CardHeader,
+  CardTitle,
+  CardDescription,
+  CardContent,
+} from '@/components/ui/card';
+
+import db from '@/db/db';
+import { formatCurrency, formatNumber } from '@/lib/formatters';
+
+async function getSalesData() {
+  const data = await db.order.aggregate({
+    _sum: { pricepaidInCents: true },
+    _count: true,
+  });
+
+  await wait(2000);
+
+  return {
+    amount: (data._sum.pricepaidInCents || 0) / 100,
+    numberOfSales: data._count,
+  };
+}
+
+function wait(duration: number) {
+  return new Promise((resolve) => setTimeout(resolve, duration));
+}
+
+async function getUserData() {
+  const [userCount, orderData] = await Promise.all([
+    db.user.count(),
+    db.order.aggregate({
+      _sum: { pricepaidInCents: true },
+    }),
+  ]);
+  return {
+    userCount,
+    averageValuePerUser:
+      userCount === 0
+        ? 0
+        : (orderData._sum.pricepaidInCents || 0) / userCount / 100,
+  };
+}
+
+async function getProductData() {
+  const [activeCount, inactiveCount] = await Promise.all([
+    db.product.count({ where: { isAvailableForPurchase: true } }),
+    db.product.count({ where: { isAvailableForPurchase: false } }),
+  ]);
+  return { activeCount, inactiveCount };
+}
+
+export default async function AdminDashBoard() {
+  const [salesData, userData, productData] = await Promise.all([
+    getSalesData(),
+    getUserData(),
+    getProductData(),
+  ]);
+
+  return (
+    <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4'>
+      <DashboardCard
+        title={'Sales'}
+        subtitle={`${formatNumber(salesData.numberOfSales)} Orders`}
+        body={formatCurrency(salesData.amount)}
+      />
+      <DashboardCard
+        title={'Customers'}
+        subtitle={`${formatCurrency(
+          userData.averageValuePerUser
+        )} Average Value`}
+        body={formatNumber(userData.userCount)}
+      />
+      <DashboardCard
+        title={'Active Products'}
+        subtitle={`${formatNumber(
+          productData.inactiveCount
+        )} Inactive Products`}
+        body={formatNumber(productData.activeCount)}
+      />
+    </div>
+  );
+}
+
+type DashBoardProps = {
+  title: string;
+  subtitle: string;
+  body: string;
+};
+
+function DashboardCard({ title, subtitle, body }: DashBoardProps) {
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle>{title}</CardTitle>
+        <CardDescription>{subtitle}</CardDescription>
+      </CardHeader>
+      <CardContent>
+        <p>{body}</p>
+      </CardContent>
+    </Card>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,7 @@
-import Image from "next/image";
-import { Button } from "@/components/ui/button";
-
 export default function Home() {
   return (
     <div>
-      <h1>Admin Page</h1>
-      <Button>Click me</Button>
+      <h1>Home Page</h1>
     </div>
-    
   );
 }
