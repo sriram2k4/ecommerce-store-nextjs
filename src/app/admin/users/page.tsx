4945df09251ed30b6f9af6a1cd35eabfd9276943
sriram2k4 <sriramthehacker01@gmail.com>
2024-05-13 18:48:00 +0530

    Admin customers page

--- src/app/admin/users/page.tsx
+++ src/app/admin/users/page.tsx
@@ -1,3 +1,81 @@
-export default function User() {
-  return <h1>User Page</h1>;
+import { PageHeader } from '../_components/PageHeader';
+import { Button } from '@/components/ui/button';
+import Link from 'next/link';
+import db from '@/db/db';
+
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from '@/components/ui/table';
+import { CheckCircle, MoreVertical, XCircle } from 'lucide-react';
+import { formatCurrency, formatNumber } from '@/lib/formatters';
+import {
+  DropdownMenu,
+  DropdownMenuTrigger,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuSeparator,
+} from '@/components/ui/dropdown-menu';
+import { DeleteDropDownItem } from './_components/UserActions';
+
+function getUsers() {
+  return db.user.findMany({
+    select: {
+      id: true,
+      email: true,
+      orders: { select: { pricepaidInCents: true } },
+    },
+    orderBy: { createdAt: 'desc' },
+  });
+}
+
+export default async function UsersTable() {
+  const users = await getUsers();
+
+  if (users.length === 0) return <p>No Customers Found</p>;
+
+  return (
+    <Table>
+      <TableHeader>
+        <TableRow>
+          <TableHead>Email</TableHead>
+          <TableHead>Orders</TableHead>
+          <TableHead>Value</TableHead>
+          <TableHead className='w-0'>
+            <span className='sr-only'>Actions</span>
+          </TableHead>
+        </TableRow>
+      </TableHeader>
+      <TableBody>
+        {users.map((user) => (
+          <TableRow key={user.id}>
+            <TableCell>{user.email}</TableCell>
+            <TableCell>{formatNumber(user.orders.length)}</TableCell>
+            <TableCell>
+              {formatCurrency(
+                user.orders.reduce((sum, o) => o.pricepaidInCents + sum, 0) /
+                  100
+              )}
+            </TableCell>
+            <TableCell>
+              <DropdownMenu>
+                <DropdownMenuTrigger>
+                  <MoreVertical />
+                  <span className='sr-only'>Actions</span>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent>
+                  <DeleteDropDownItem id={user.id} />
+                </DropdownMenuContent>
+              </DropdownMenu>
+            </TableCell>
+          </TableRow>
+        ))}
+      </TableBody>
+    </Table>
+  );
 }
